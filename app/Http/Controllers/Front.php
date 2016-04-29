871fb4bfb33854a35f7d693e446b0641e4226255
Nhut Tran <nhuttranm@gmail.com>
2016-04-29 17:52:54 +0700

    new larashop

---
+++ app/Http/Controllers/Front.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class Front extends Controller
+{
+    public function index() {
+        return view('home', array('page' => 'home'));
+    }
+
+    public function products() {
+        return view('products', array('page' => 'products'));
+    }
+
+    public function product_details($id) {
+        return view('product_details', array('page' => 'products'));
+    }
+
+    public function product_categories($name) {
+        return view('products', array('page' => 'products'));
+    }
+
+    public function product_brands($name, $category = null) {
+        return view('products', array('page' => 'products'));
+    }
+
+    public function blog() {
+        return view('blog', array('page' => 'blog'));
+    }
+
+    public function blog_post($id) {
+        return view('blog_post', array('page' => 'blog'));
+    }
+
+    public function contact_us() {
+        return view('contact_us', array('page' => 'contact_us'));
+    }
+
+    public function login() {
+        return view('login', array('page' => 'home'));
+    }
+
+    public function logout() {
+        return view('login', array('page' => 'home'));
+    }
+
+    public function cart() {
+        return view('cart', array('page' => 'home'));
+    }
+
+    public function checkout() {
+        return view('checkout', array('page' => 'home'));
+    }
+
+    public function search($query) {
+        return view('products', array('page' => 'products'));
+    }
+
+}
